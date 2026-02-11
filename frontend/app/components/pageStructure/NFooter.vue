bfa377211fbad4bacff25e3bf320d1130207b87d
Abrams <yuanchianchen@gmail.com>
2026-02-11 18:29:16 +0800

    add: using tailwind

--- frontend/app/components/pageStructure/NFooter.vue
+++ frontend/app/components/pageStructure/NFooter.vue
@@ -1,23 +1,23 @@
 <template>
-	<footer>
-		<div id="top">
-			<div class="topDiv" id="left">
-				<div id="lefttop">
-					<img id="footerLogo" src="/pageStructure/dsseLogo.png" alt="" />
-					<div id="footerSchoolName">
-						<p id="chinese">國立中央大學太空科學與工程學系</p>
+	<footer class="w-screen h-auto bg-(--color1) text-(--color4) p-8 pb-1 flex flex-col items-center justify-center">
+		<div class="w-full h-auto flex flex-row items-stretch justify-between">
+			<div class="w-1/2 h-auto flex flex-col items-start justify-between">
+				<div class="w-full h-auto flex flex-row items-center justify-left gap-3">
+					<img class="w-1/9 aspect-1:1" src="/pageStructure/dsseLogo.png" alt="" />
+					<div class="w-auto h-auto flex flex-col items-start justify-center">
+						<p class="text-2xl font-bold">國立中央大學太空科學與工程學系</p>
 						<p id="english">Department of Space Science and Engineering, NCU</p>
 					</div>
 				</div>
-				<ul>
+				<ul class="text-[0.9rem]">
 					<li>地址:桃園市中壢區中大路300號</li>
 					<li>Address: No. 300, Zhongda Rd., Zhongli District, Taoyuan City 32001, Taiwan</li>
 					<li>TEL:+886-3-4227151 Ext.65750</li>
 					<li>FAX:+886-3-4224394</li>
 				</ul>
 			</div>
-			<div class="topDiv">
-				<p class="title">Pages</p>
+			<div class="w-1/5 h-auto flex flex-col items-start justify-start">
+				<p class="font-bold text-[1.2rem] mb-2">Pages</p>
 				<ul>
 					<li>
 						<a href="/"><i class="fa-solid fa-house"></i>首頁 Home</a>
@@ -39,8 +39,8 @@
 					</li>
 				</ul>
 			</div>
-			<div class="topDiv">
-				<p class="title">Quick Links</p>
+			<div class="w-1/6 h-auto flex flex-col items-start justify-start">
+				<p class="font-bold text-[1.2rem] mb-2">Quick Links</p>
 				<ul>
 					<li>
 						<a href="https://www.ncu.edu.tw">中大校網</a>
@@ -57,92 +57,8 @@
 				</ul>
 			</div>
 		</div>
-		<div id="copyright">
-			<p>Copyright © 2026 Abrams. All rights reserved.</p>
+		<div class="w-full mt-4 border-t border-(--color4) flex items-center justify-center">
+			<p class="text-sm">Copyright © 2026 Abrams. All rights reserved.</p>
 		</div>
 	</footer>
 </template>
-
-<script setup></script>
-
-<style scoped>
-footer {
-	width: calc(100vw - 70px);
-	height: auto;
-	background-color: var(--color1);
-	padding: 35px;
-	padding-bottom: 5px;
-	color: var(--color4);
-	font-size: 0.8rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-}
-a {
-	color: var(--color4);
-}
-ul {
-	margin-top: 10px;
-}
-#top {
-	width: 100%;
-	display: flex;
-	flex-direction: row;
-	justify-content: space-evenly;
-	align-items: stretch;
-	gap: 50px;
-}
-.topDiv {
-	width: 25%;
-	display: flex;
-	flex-direction: column;
-	align-items: start;
-	justify-content: start;
-}
-#copyright {
-	width: 100%;
-	margin-top: 15px;
-	border-top: 1px solid var(--color4);
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
-#left {
-	width: 50%;
-	display: flex;
-	flex-direction: column;
-	align-items: start;
-	justify-content: space-between;
-}
-#lefttop {
-	width: 100%;
-	height: auto;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: start;
-}
-#footerLogo {
-	width: 10%;
-	aspect-ratio: 1/1;
-	margin-right: 10px;
-}
-#footerSchoolName {
-	width: calc(90% - 10px);
-	height: auto;
-	display: flex;
-	flex-direction: column;
-	align-items: start;
-	justify-content: center;
-}
-#chinese {
-	width: 100%;
-	font-size: 1.5rem;
-	font-weight: bold;
-}
-.title {
-	font-size: 1rem;
-	font-weight: bold;
-}
-</style>
--- frontend/app/components/pageStructure/NHeader.vue
+++ frontend/app/components/pageStructure/NHeader.vue
@@ -1,65 +1,21 @@
 <template>
-	<header>
-		<a href="/" id="logoLink"><img src="/pageStructure/dsseLogo.png" alt="" /></a>
-		<ul>
-			<li>
-				<a href="/"><i class="fa-solid fa-house"></i>首頁 Home</a>
-			</li>
-			<li>
-				<a href="#"><i class="fa-solid fa-circle-info"></i>簡介 Introduction</a>
-			</li>
-			<li>
-				<a href="#"><i class="fa-solid fa-people-group"></i>人員 Faculty</a>
-			</li>
-			<li>
-				<a href="#"><i class="fa-solid fa-ticket"></i>招生 Admission</a>
-			</li>
-			<li>
-				<a href="#"><i class="fa-solid fa-gavel"></i>規章 Regulation</a>
-			</li>
-			<li>
-				<a href="#"><i class="fa-solid fa-trophy"></i>成就 Achievement</a>
+	<header class="w-screen h-20 bg-(--color1) p-4 fixed top-0 left-0 z-50 flex flex-row items-center justify-between">
+		<a href="/" class="h-12 aspect-1:1"><img src="/pageStructure/dsseLogo.png" class="h-full aspect-1:1" alt="" /></a>
+		<ul class="h-full w-auto flex flex-row justify-center items-center gap-5">
+			<li v-for="(link, index) in pages" :key="index">
+				<a :href="link.url" class="text-base text-(--color4)"><i :class="link.icon"></i>{{ link.name }}</a>
 			</li>
 		</ul>
 	</header>
 </template>
 
-<script setup></script>
-
-<style scoped>
-header {
-	width: calc(100vw - 30px);
-	height: 8vh;
-	background-color: var(--color1);
-	padding: 15px;
-	position: fixed;
-	top: 0;
-	left: 0;
-	z-index: 50;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: space-between;
-}
-#logoLink {
-	height: 100%;
-	aspect-ratio: 1/1;
-}
-#logoLink img {
-	height: 100%;
-	aspect-ratio: 1/1;
-}
-ul {
-	height: 100%;
-	width: auto;
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-	align-items: center;
-	gap: 20px;
-}
-a {
-	color: var(--color4);
-	font-size: 1rem;
-}
-</style>
+<script setup>
+const pages = [
+	{ name: "首頁 Home", url: "/", icon: "fa-solid fa-house" },
+	{ name: "簡介 Introduction", url: "#", icon: "fa-solid fa-circle-info" },
+	{ name: "人員 Faculty", url: "#", icon: "fa-solid fa-people-group" },
+	{ name: "招生 Admission", url: "#", icon: "fa-solid fa-ticket" },
+	{ name: "規章 Regulation", url: "#", icon: "fa-solid fa-gavel" },
+	{ name: "成就 Achievement", url: "#", icon: "fa-solid fa-trophy" },
+];
+</script>
