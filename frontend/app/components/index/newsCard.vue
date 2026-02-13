c0f185069751ccb0e9124d476c7541c5840bc509
Abrams <yuanchianchen@gmail.com>
2026-02-13 13:48:29 +0800

    feat: add news pages on index

---
+++ frontend/app/components/index/newsCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="h-[15%] w-full p-3 border-b border-[var(--color4)] flex flex-row items-center justify-between gap-5">
+		<p class="w-[5%] flex flex-row items-center justify-start gap-2 font-bold" :id="`tag-${props.news.id}`">{{ props.news.tag }}</p>
+		<h3 class="w-[75%] overflow-hidden text-[var(--color4)]">{{ props.news.title }}</h3>
+		<p class="w-[10%] text-[var(--color4)] flex flex-row justify-end">{{ props.news.date }}</p>
+		<a class="w-[10%] hover:underline flex flex-row justify-end text-[var(--color4)]">View Detail ></a>
+	</div>
+</template>
+
+<script setup>
+import newsCatalog from "~/assets/json/newsCatalog.json";
+
+const props = defineProps({
+	news: Object,
+});
+
+onMounted(() => {
+	const tagIndex = newsCatalog.tags.indexOf(props.news.tag);
+	let tag = document.getElementById(`tag-${props.news.id}`);
+	tag.style.color = newsCatalog.colors[tagIndex];
+});
+</script>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -9,14 +9,28 @@
 			We're<textTyping :text="['Students', 'Scientists', 'Engineers', 'Future']" :time="10000" />
 		</h1>
 	</section>
+	<section class="w-screen h-screen flex items-center justify-center p-10" id="news">
+		<div class="w-full h-full backdrop-blur-md rounded-xl p-10 flex flex-col items-start justify-between gap-5">
+			<div class="w-full h-[10%] flex flex-row items-center justify-between">
+				<h1 class="text-[var(--color4)] font-bold text-3xl">最新消息NEWS</h1>
+				<a href="#" class="w-32 h-8 text-[var(--color1)] bg-[var(--color3)] rounded-full flex items-center justify-center">View More</a>
+			</div>
+			<div class="w-full h-[85%] border-[1px] border-[var(--color4)] rounded-md pl-3 pr-3 flex flex-col items-start justify-between">
+				<newsCard v-for="xnews in news" :news="xnews"/>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script setup>
 import textTyping from "~/components/index/textTyping.vue";
+import newsCard from "~/components/index/newsCard.vue";
 
 import emergencyInfo from "~/assets/json/emergencyInfo.json";
+import newsData from "~/assets/json/news.json";
 
 const showEmergencyInfo = ref(false);
+const news=ref([]);
 
 const closeEmergencyInfo = () => {
 	showEmergencyInfo.value = false;
@@ -25,11 +39,20 @@ const closeEmergencyInfo = () => {
 if (emergencyInfo.info !== "") {
 	showEmergencyInfo.value = true;
 }
+
+for (let i = newsData.length-1; i>=newsData.length-6 && i>=0; i--) {
+		news.value.push(newsData[i]);
+}
 </script>
 
 <style scoped>
 #bg {
 	background-image: url("/index/bg1.jpg");
+	background-size: cover;
+	background-position: top;
+}
+#news {
+	background-image: url("/index/bg2.jpg");
 	background-size: cover;
 	background-position: top;
 }
