d640eab4183953a0fa022baa69edee7f6ce25c60
Abrams <yuanchianchen@gmail.com>
2026-02-15 10:24:03 +0800

    add: media page

---
+++ frontend/app/pages/media.vue
@@ -0,0 +1,34 @@
+<template>
+	<section class="w-screen h-[50vh] flex items-center justify-center pt-20" id="bg">
+		<h1 class="text-[var(--color4)] lg:text-[5vw] text-[10vw] font-bold">媒體 Media</h1>
+	</section>
+	<section class="w-screen h-auto bg-[var(--color1)] lg:p-20 p-5 flex flex-col gap-[8vh]">
+		<div class="w-full h-auto flex flex-row justify-around flex-wrap gap-10">
+			<a href="#" v-for="med in media" class="lg:w-[300px] w-full h-auto flex flex-col gap-0 text-[var(--color4)]">
+				<img class="img w-full aspect-video rounded-md" :src="med.imgSrc" alt="" />
+				<br />
+				<p class="w-full text-[var(--color3)]">{{ med.date }}</p>
+				<p class="w-full text-[var(--color4)]">{{ med.title }}</p>
+			</a>
+		</div>
+	</section>
+</template>
+
+<script setup>
+import media from "~/assets/json/media.json";
+
+const props = defineProps({
+	isMobile: Boolean,
+});
+</script>
+
+<style scoped>
+#bg {
+	background-image: url("/media/bg1.jpg");
+	background-position: center;
+	background-size: cover;
+}
+.img {
+	object-fit: cover;
+}
+</style>
--- frontend/app/pages/regulation.vue
+++ frontend/app/pages/regulation.vue
@@ -1,13 +1,13 @@
 <template>
 	<section class="w-screen h-[50vh] flex items-center justify-center pt-20" id="bg">
-		<h1 class="text-[var(--color4)] text-[5vw] font-bold">規章 Regulation</h1>
+		<h1 class="text-[var(--color4)] lg:text-[5vw] text-[10vw] font-bold">規章 Regulation</h1>
 	</section>
-	<section class="w-screen h-auto bg-[var(--color1)] p-20 flex flex-col gap-[8vh]">
+	<section class="w-screen h-auto bg-[var(--color1)] lg:p-20 p-5 flex flex-col gap-[8vh]">
 		<div v-for="doc in documents" class="w-full h-auto flex flex-col gap-3 text-[var(--color4)]">
 			<h2 class="text-3xl font-bold">{{ doc.CatalogName }}</h2>
 			<div class="w-full h-auto flex flex-col gap-3">
 				<div v-for="link in doc.docs" class="w-full h-15 flex flex-row justify-between">
-					<a class="w-[80%] h-full bg-[var(--color2)] p-5 flex flex-row items-center justify-start" :href="link.docLink"
+					<a class="w-[80%] h-full bg-[var(--color2)] p-5 flex flex-row gap-2 items-center justify-start" :href="link.docLink"
 						><font-awesome-icon icon="fa-solid fa-download" />{{ link.docName }}</a
 					>
 					<div class="w-[19%] h-full bg-[var(--color3)] p-5 flex flex-row items-center justify-center text-[var(--color1)]">
